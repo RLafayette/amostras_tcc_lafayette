aac0b6e96fc6522472077de342c7935c1d969257
Rodrigo Lafayette <rodrigolafayette@hotmail.com>
2020-08-24 04:18:19 -0300

    Amostras pequenas, incluindo adaptações da QuixBugs

---
+++ bad_inheritance/src/main/ExceptionThrower.java
@@ -0,0 +1,7 @@
+package main;
+
+public class ExceptionThrower {
+	public void doSomething() {
+		throw new NullReferenceException();
+	}
+}
---
+++ bad_inheritance/src/main/NullReferenceException.java
@@ -0,0 +1,5 @@
+package main;
+
+public class NullReferenceException extends NullPointerException {
+
+}
